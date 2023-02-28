826f0a6c1630806746de7cadc5385257b05651b9
Syed Aatif Hasan <syedaatifhasan581@gmail.com>
2023-02-28 10:44:55 +0545

    Version

---
+++ 3rd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int i=1;
+    int sum=0;
+    for(i=0;i<=10;i++)
+    {
+        sum+=i;
+    }
+    printf("sum=%d",sum);
+}
--- AatifV1.c
+++ AatifV1.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    int i;
-    for(i=0;i<=10;)
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++)
     {
-        i=i++;
+       sum+=i;
     }
-    printf("%d,",i);
+    printf("Sum=%d",sum);
 }
